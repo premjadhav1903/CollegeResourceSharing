084032806dcbfe279c9fe8ec82b61a4c8ba35239
premjadhav1903 <premjadhav1903@gmail.com>
2026-07-17 16:39:20 +0530

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,13 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" College Resource Sharing System");
+        System.out.println("==================================");
+
+        // Application starts here
+
+    }
+
+}
